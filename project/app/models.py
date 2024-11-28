c9c210cd193366d7d59bfef1b26abf38dff245ab
brajesh708 <brajeshmewada694@gmail.com>
2024-11-28 22:52:31 +0530

    update

--- project/app/models.py
+++ project/app/models.py
@@ -14,3 +14,12 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.user.name
+    
