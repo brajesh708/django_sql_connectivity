ba6a7ddf73ed4ab4cd157c802642d2b50dada0ae
brajesh708 <brajeshmewada694@gmail.com>
2024-11-28 22:56:26 +0530

    update

--- project/app/models.py
+++ project/app/models.py
@@ -20,6 +20,10 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    def __str__(self):
-        return self.user.name
     
+
+# Create a logger
+
+
+
+
